1302b87259424b5985d683f0cb015ed49aaf67b7
Maaz Bin Asad <60134184+maaz-bin-asad@users.noreply.github.com>
2020-05-22 16:09:41 +0530

    Add files via upload

---
+++ odd-even.py
@@ -0,0 +1,9 @@
+#the given program is for brick sort/odd even sort
+arr=[2,1,3]
+for i in range(0,len(arr)-1,2):
+	if(arr[i+1]<arr[i]):
+		arr[i+1],arr[i]=arr[i],arr[i+1]
+for i in range(1,len(arr)-1,2):
+	if arr[i+1]<arr[i]:
+		arr[i+1],arr[i]=arr[i],arr[i+1]
+print(arr)
