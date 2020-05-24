adc44bfc6fa76d4e94a06f258e0b58fea207f3b6
Apoorve73 <apoorve73@gmail.com>
2020-05-24 15:09:33 +0530

    Selection_sort_Apoorve73

---
+++ Selection Sort/SelectionSort.py
@@ -0,0 +1,24 @@
+#Selection Sort to find to sort an unsorted array
+
+def SelectionSort(arr):
+	# Traverse through all array elements 
+	for i in range(len(arr)):
+
+		# Find the minimum element in remaining 
+	 	# unsorted array
+		min_index = i
+		for j in range(i+1, len(arr)):
+			if arr[min_index] > arr[j]:
+				min_index = j
+
+		# Swap the found minimum element with 
+		# the first element	
+		arr[min_index], arr[i] = arr[i], arr[min_index]
+
+	print(arr)
+if __name__ == "__main__":
+
+    #input an unsorted array
+    arr = [int(x) for x in input().split()]
+	#Function Call
+    SelectionSort(arr)
