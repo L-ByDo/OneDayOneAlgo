8b84051fdad1b14062a13184364a70c1b5381426
Maaz Bin Asad <60134184+maaz-bin-asad@users.noreply.github.com>
2020-07-02 00:28:05 +0530

    Add coin change problem/ Maaz

---
+++ Dynamic Programming/coin_change_problem.cpp
@@ -0,0 +1,22 @@
+int count( int S[], int m, int n ) 
+{ 
+	// table[i] will be storing the number of solutions for 
+	// value i. We need n+1 rows as the table is constructed 
+	// in bottom up manner using the base case (n = 0) 
+	int table[n+1]; 
+
+	// Initialize all table values as 0 
+	memset(table, 0, sizeof(table)); 
+
+	// Base case (If given value is 0) 
+	table[0] = 1; 
+
+	// Pick all coins one by one and update the table[] values 
+	// after the index greater than or equal to the value of the 
+	// picked coin 
+	for(int i=0; i<m; i++) 
+		for(int j=S[i]; j<=n; j++) 
+			table[j] += table[j-S[i]]; 
+
+	return table[n]; 
+} 
