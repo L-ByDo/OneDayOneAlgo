563472d34dc68d9373404f45ed089b5720c2dac3
Maaz Bin Asad <60134184+maaz-bin-asad@users.noreply.github.com>
2020-07-03 23:23:13 +0530

    Add activity selection/ Maaz

---
+++ Dynamic Programming/Activity_Selection_OOPS.py
@@ -0,0 +1,23 @@
+class A:
+    def __init__(self,starting,finishing):
+        self.starting=starting
+        self.finishing=finishing
+    def __lt__(self,other): #operator overloading to sort objects according to finish time
+        return self.finishing < other.finishing
+class Find:
+    
+    def cal(self,start,finish):
+        value=[]
+        for i in range(0,len(start)):
+            value.append(A(start[i],finish[i]))  #assigning instance variables to objects
+        value.sort()
+        count=1
+        for i in range(1,len(value)):
+            if value[i].starting>=value[i-1].finishing:  #comparing using finishing time
+                count+=1
+        return count
+
+start=[1,3,0,5,8,5]
+finish=[2,4,6,7,9,9]
+obj=Find()
+print(obj.cal(start,finish))
