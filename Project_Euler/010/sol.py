539485fd9f40f6f971c47b408f3f3e1bbcd03460
Restyled.io <commits@restyled.io>
2020-09-30 20:52:41 +0000

    Restyled by autopep8

--- Project_Euler/010/sol.py
+++ Project_Euler/010/sol.py
@@ -1,24 +1,25 @@
-def sieve(n): 
+def sieve(n):
 
     sum = 0
-    prime = [True for i in range(n+1)] 
+    prime = [True for i in range(n+1)]
     p = 2
-    while (p * p <= n): 
-          
-        # If prime[p] is not changed, then it is a prime 
-        if (prime[p] == True): 
-              
-            # Update all multiples of p 
-            for i in range(p * p, n+1, p): 
+    while (p * p <= n):
+
+        # If prime[p] is not changed, then it is a prime
+        if (prime[p] == True):
+
+            # Update all multiples of p
+            for i in range(p * p, n+1, p):
                 prime[i] = False
         p += 1
-      
-    # sum up all prime numbers 
-    for p in range(2, n+1): 
-        if prime[p]: 
+
+    # sum up all prime numbers
+    for p in range(2, n+1):
+        if prime[p]:
             sum = sum + p
-            
+
     print(sum)
-  
+
+
 for _ in range(int(input())):
-    sieve(int(input())) 
+    sieve(int(input()))
