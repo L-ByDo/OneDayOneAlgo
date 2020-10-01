ad82d0f43056430fbcbc6e6fb75def4e4004164d
Restyled.io <commits@restyled.io>
2020-10-01 14:57:25 +0000

    Restyled by autopep8

--- Algorithms/General_Algorithms/Sieve_Of_Eratosthenes/Python/Seive of Eratosthenes.py
+++ Algorithms/General_Algorithms/Sieve_Of_Eratosthenes/Python/Seive of Eratosthenes.py
@@ -1,14 +1,16 @@
-#To print all prime numbers from [2,n] for given n in the most efficient seive of eratosthenes algo
+# To print all prime numbers from [2,n] for given n in the most efficient seive of eratosthenes algo
 import math
 
-def SeiveOfEratosthenes(Num):
-    prime = [True for num in range(0, Num+1)] #Set all values from 2 to n as True
-    prime[1] = prime[0] = False
-    num = 2 
 
-    #Check for square of num if it is less than the root of Num. This is seive till root approach.
+def SeiveOfEratosthenes(Num):
+    # Set all values from 2 to n as True
+    prime = [True for num in range(0, Num+1)]
+    prime[1] = prime[0] = False
+    num = 2
+
+    # Check for square of num if it is less than the root of Num. This is seive till root approach.
     #
-    while  num*num <= int(math.sqrt(Num)) :
+    while num*num <= int(math.sqrt(Num)):
         if prime[num]:
             for i in range(num*num, Num+1, num):
                 prime[i] = False
@@ -20,7 +22,8 @@ def SeiveOfEratosthenes(Num):
             print(i)
     return count
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     Num = int(input())
     count = SeiveOfEratosthenes(Num)
     print(count)
--- Algorithms/Greedy_Algorithms/Knapsack/Python/Knapsack_2.py
+++ Algorithms/Greedy_Algorithms/Knapsack/Python/Knapsack_2.py
@@ -1,43 +1,49 @@
 class total:
-    def __init__(self,weight,value,index):   #setting instance variables to objects
-        self.weight=weight
-        self.value=value
-        self.index=index
-        self.cost=value/weight
+    def __init__(self, weight, value, index):  # setting instance variables to objects
+        self.weight = weight
+        self.value = value
+        self.index = index
+        self.cost = value/weight
 
-    def __lt__(self, other):   #operator overloading to compare two objects' costs. Called whenever objects will be
+    def __lt__(self, other):  # operator overloading to compare two objects' costs. Called whenever objects will be
         # comapred
         return self.cost < other.cost
 
+
 class final_value:
-    @staticmethod  #can be used by any member thats why called static method
-    def get_max(weights,values):
-        max_weight_per_unit=[]
-        for i in range(0,len(weights)):
-            max_weight_per_unit.append(total(weights[i],values[i],i))
+    @staticmethod  # can be used by any member thats why called static method
+    def get_max(weights, values):
+        max_weight_per_unit = []
+        for i in range(0, len(weights)):
+            max_weight_per_unit.append(total(weights[i], values[i], i))
         print(max_weight_per_unit)
-        #initialize instance variables of objects and place them in this list
-        max_weight_per_unit.sort(reverse=True)  #sort the objects
+        # initialize instance variables of objects and place them in this list
+        max_weight_per_unit.sort(reverse=True)  # sort the objects
         print(max_weight_per_unit)
         for objct in max_weight_per_unit:
             print(objct.value, objct.weight, objct.cost)
 
-        total_value=0 
-        capacity=50
+        total_value = 0
+        capacity = 50
 
         for objct in max_weight_per_unit:
-            
-            if capacity>=int(objct.weight):   #if more capacity than current object's weight, then simply add the value
+
+            # if more capacity than current object's weight, then simply add the value
+            if capacity >= int(objct.weight):
                 # to total value
-                total_value=total_value+int(objct.value)
-                capacity-=int(objct.weight)
+                total_value = total_value+int(objct.value)
+                capacity -= int(objct.weight)
 
             else:
-                total_value+=int(objct.value)*(int(capacity)/int(objct.weight))  #else add some part of value of the object
+                # else add some part of value of the object
+                total_value += int(objct.value) * \
+                    (int(capacity)/int(objct.weight))
                 break
 
         return total_value
-weights=[10,40,20,30]
-values=[60,40,100,120]
-max_value=final_value.get_max(weights,values)
+
+
+weights = [10, 40, 20, 30]
+values = [60, 40, 100, 120]
+max_value = final_value.get_max(weights, values)
 print(max_value)
