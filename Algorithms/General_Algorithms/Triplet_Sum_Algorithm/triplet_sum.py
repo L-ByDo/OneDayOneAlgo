00f25298334cd16bf67c10a008533b223e39d5b3
Amit Kumar Agarwal <amitagarwal.aa57@gmail.com>
2020-10-03 10:34:13 +0530

    Update triplet_sum.py

--- Algorithms/General_Algorithms/Triplet_Sum_Algorithm/triplet_sum.py
+++ Algorithms/General_Algorithms/Triplet_Sum_Algorithm/triplet_sum.py
@@ -1,30 +1,49 @@
 # Python3 program to find a triplet 
-# that sum to a given value 
-
-# returns true if there is triplet with 
-# sum equal to 'sum' present in A[]. 
-# Also, prints the triplet 
+  
+# returns true if there is triplet 
+# with sum equal to 'sum' present 
+# in A[]. Also, prints the triplet 
 def find3Numbers(A, arr_size, sum): 
-
-	# Fix the first element as A[i] 
-	for i in range( 0, arr_size-2): 
-
-		# Fix the second element as A[j] 
-		for j in range(i + 1, arr_size-1): 
-			
-			# Now look for the third number 
-			for k in range(j + 1, arr_size): 
-				if A[i] + A[j] + A[k] == sum: 
-					print("Triplet is", A[i], 
-						", ", A[j], ", ", A[k]) 
-					return True
-	
-	# If we reach here, then no 
-	# triplet was found 
-	return False
-
-# Driver program to test above function 
+  
+    # Sort the elements  
+    A.sort() 
+  
+    # Now fix the first element  
+    # one by one and find the 
+    # other two elements  
+    for i in range(0, arr_size-2): 
+      
+  
+        # To find the other two elements, 
+        # start two index variables from 
+        # two corners of the array and 
+        # move them toward each other 
+          
+        # index of the first element 
+        # in the remaining elements 
+        l = i + 1 
+          
+        # index of the last element 
+        r = arr_size-1 
+        while (l < r): 
+          
+            if( A[i] + A[l] + A[r] == sum): 
+                print("Triplet is", A[i],  
+                     ', ', A[l], ', ', A[r]); 
+                return True
+              
+            elif (A[i] + A[l] + A[r] < sum): 
+                l += 1
+            else: # A[i] + A[l] + A[r] > sum 
+                r -= 1
+  
+    # If we reach here, then 
+    # no triplet was found 
+    return False
+  
+# Driver program to test above function  
 A = [1, 4, 45, 6, 10, 8] 
 sum = 22
 arr_size = len(A) 
-find3Numbers(A, arr_size, sum) 
+  
+find3Numbers(A, arr_size, sum)
